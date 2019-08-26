4e548444e81885f8543eebaca94b2a6716820d87
Michael Hinte <michaelhinte@Michaels-MacBook-Pro-2.local>
2019-08-26 11:55:18 -0500

    creates projects show page

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,7 +2,33 @@ const Journal = require('../models/journal');
 
 module.exports = {
     new: newProj,
+    show,
+    newNote
 };
+
+function newNote(req, res, next) {
+    Journal.findOne({'projects._id': req.params.id}, (err, journal) =>
+    {if (err) throw err;
+        var elem = journal.projects.map(function (x) {
+            return x.id;
+        }).indexOf(req.params.id);
+        var proj = journal.projects[elem];
+        proj.notes.push[req.body];
+       proj.save
+    });
+}
+
+
+function show(req, res, next) {
+    Journal.findOne({'projects._id': req.params.id}, (err, journal) =>
+    {if (err) throw err;
+        var elem = journal.projects.map(function (x) {
+            return x.id;
+        }).indexOf(req.params.id);
+        var proj = journal.projects[elem];
+    res.render('projects/show', {journal, proj})}
+     )
+}
 
 
 function newProj(req, res) {
@@ -10,3 +36,13 @@ function newProj(req, res) {
         res.render('projects/new', { journal });
     })
 }
+
+
+// function show(req, res, next) {
+    
+//     Journal.findOne({'projects._id': req.params.id}, function(err, journal) {
+        
+//         let id = req.params.id
+//         res.render(`projects/show`, { journal, id});
+//     })
+// }
--- routes/projects.js
+++ routes/projects.js
@@ -4,5 +4,9 @@ const projectsCtrl = require('../controllers/projects');
 
 router.get('/journals/:id/new', projectsCtrl.new);
 
+router.get('/projects/:id', projectsCtrl.show);
+
+router.post('/projects/:id', projectsCtrl.newNote);
+
 
 module.exports = router;
